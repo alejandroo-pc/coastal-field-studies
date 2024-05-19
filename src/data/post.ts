fe8c7478036332abda35223433b5d0d3d904e164
alejandro-pc <alejandropatinoc@gmail.com>
2024-05-18 17:10:15 -0700

    filter notes by oldest to newest

--- src/data/post.ts
+++ src/data/post.ts
@@ -12,7 +12,7 @@ export function sortMDByDate(posts: Array<CollectionEntry<"post">>) {
 	return posts.sort((a, b) => {
 		const aDate = new Date(a.data.updatedDate ?? a.data.publishDate).valueOf();
 		const bDate = new Date(b.data.updatedDate ?? b.data.publishDate).valueOf();
-		return bDate - aDate;
+		return aDate - bDate;
 	});
 }
 
